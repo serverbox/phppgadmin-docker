3a85fb839d1a9cc2df2ee8da462d40598881763f
Timothy Clarke <Timothy.Clarke@ServerBox.co.uk>
2018-03-09 10:02:27 +0000

    nginx logs via console, also x-forwarded for

--- etc/config.inc.php
+++ etc/config.inc.php
@@ -1,5 +1,5 @@
 <?php
-error_reporting(E_ALL & ~E_NOTICE & ~E_DEPRECATED);
+error_reporting(E_ALL & ~E_DEPRECATED);
 
 /*
  * See https://github.com/phppgadmin/phppgadmin/blob/master/conf/config.inc.php-dist
